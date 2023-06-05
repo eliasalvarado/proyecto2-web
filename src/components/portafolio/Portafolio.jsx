ec02678140a4d4b9b29cebf68e27e317ec7d8e8e
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2023-06-05 01:51:56 -0600

    Ajustes al componente Portafolio

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import styles from './Portafolio.module.css'
 import Proyecto from '../proyecto/Proyecto'
+import imgFox from '../../assets/lab4.png'
+import imgKNY from '../../assets/lab6.png'
+import imgSP from '../../assets/spotify.png'
+import imgMV from '../../assets/moview.jpg'
+import imgCL from '../../assets/clinica.png'
 
 function Portafolio() {
   const [proyectos, setProyectos] = useState([])
 
   const proyectosDemo = [
     {
-      name: 'Minecraft Fox', img: '/src/assets/lab4.png', link: 'https://uvgenios.online/21808/lab4/', github: 'https://github.com/eliasalvarado/lab4-cssOnly.git', technologys: ['html', 'css'],
+      name: 'Minecraft Fox', img: `${imgFox}`, link: 'https://uvgenios.online/21808/lab4/', github: 'https://github.com/eliasalvarado/lab4-cssOnly.git', technologys: ['html', 'css'],
     },
     {
-      name: "Kimetsu No Yaiba's Memory", img: '/src/assets/lab6.png', link: 'https://uvgenios.online/21808/lab6/', github: 'https://github.com/eliasalvarado/lab6.git', technologys: ['js', 'react', 'css'],
+      name: "Kimetsu No Yaiba's Memory", img: `${imgKNY}`, link: 'https://uvgenios.online/21808/lab6/', github: 'https://github.com/eliasalvarado/lab6.git', technologys: ['js', 'react', 'css'],
     },
     {
-      name: 'Replicación de Spotify', img: '/src/assets/spotify.png', link: 'https://uvgenios.online/21808/spotify/', github: 'https://github.com/eliasalvarado/proyecto1-web.git', technologys: ['js', 'react', 'css'],
+      name: 'Replicación de Spotify', img: `${imgSP}`, link: 'https://uvgenios.online/21808/spotify/', github: 'https://github.com/eliasalvarado/proyecto1-web.git', technologys: ['js', 'react', 'css'],
     },
     {
-      name: 'Moview', img: '/src/assets/moview.jpg', link: 'https://github.com/eliasalvarado/Moview.git', github: 'https://github.com/eliasalvarado/Moview.git', technologys: ['kotlin'],
+      name: 'Moview', img: `${imgMV}`, link: 'https://github.com/eliasalvarado/Moview.git', github: 'https://github.com/eliasalvarado/Moview.git', technologys: ['kotlin'],
     },
     {
-      name: 'Reportería de centros clínicos', img: '/src/assets/clinica.png', link: 'https://github.com/erickguerra22/DBProject_2.git', github: 'https://github.com/erickguerra22/DBProject_2.git', technologys: ['js', 'react', 'css', 'postgresql'],
+      name: 'Reportería de centros clínicos', img: `${imgCL}`, link: 'https://github.com/erickguerra22/DBProject_2.git', github: 'https://github.com/erickguerra22/DBProject_2.git', technologys: ['js', 'react', 'css', 'postgresql'],
     },
   ]
 
